c2b87c718117d90d10c3cf6d61c9109c9f512cc4
Scott Bollinger <kfalconspb@bollingerusa.com>
2014-02-20 00:56:50 -0800

    Check version and commenting in PS

---
+++ CheckVersion.ps1
@@ -0,0 +1,6 @@
+# CheckVersion.ps1
+# Checking the version of PowerShell available
+# Scott Bollinger / kfalconspb
+
+$PSVersionTable.PSVersion # $Host.Version will only show Host version only not engine.
+
---
+++ comments.ps1
@@ -0,0 +1,14 @@
+# comments.ps1
+# Commenting in PowerShell
+# Scott Bollinger / kfalconspb
+
+# The pound/hash symbol is used to comment
+
+# Starting in PowerShell version 2 <# comment #> can be used
+
+<# Can 
+be used for
+multi-line
+comments#>
+
+$PSVersionTable.PSVersion # Comments can be used on the same line as commands
