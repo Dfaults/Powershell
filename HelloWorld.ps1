f40b28b26e50912df24141631c573f9ce4055247
Scott Bollinger <kfalconspb@bollingerusa.com>
2014-02-19 22:04:37 -0800

    HelloWorld

---
+++ HelloWorld.ps1
@@ -0,0 +1,9 @@
+# HelloWorld.ps1
+# five different way to display Hello, World!
+# Scott Bollinger / kfalconspb
+
+'Hello, World'
+"Hello, World!"
+Write-Host 'Hello,World!'
+Write-Output "Hello, World! `n" # If double quotes are used you can used a tack 'n for a new line
+echo 'Hello, World!'
